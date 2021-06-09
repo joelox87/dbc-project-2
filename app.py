b1b6325e9246417668a0cbbc09beadde05c61e1c
joelox87 <j.aponte87@gmail.com>
2021-06-08 21:48:46 -0500

    updates

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     
-    return render_template("index.html")
+    return render_template("index_joel.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
