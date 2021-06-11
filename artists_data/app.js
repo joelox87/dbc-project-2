2030e5423b886ff3b9b3fa6c9fc7f51d3defa14e
Nour hamdan <78324732+nourhamdan@users.noreply.github.com>
2021-06-10 21:47:23 -0500

    Add files via upload

---
+++ artists_data/app.js
@@ -0,0 +1,92 @@
+function init() {
+    // Create a map object
+    var myMap = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 5
+    });
+
+    // Add a tile layer
+    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    }).addTo(myMap);
+
+    // An array containing each city's name, location, and population
+    var artists = [{
+        location: [53.7270, -1.8575],
+        name: "Ed Sheeran",
+        followers: "78,900,234",
+        song: "Shape of You"
+    },
+    {
+        location: [26.3683, -80.1289],
+        name: "Ariana Grande",
+        followers: "61,301,006",
+        song: " 7 rings"
+    },
+    {
+        location: [43.6532, -79.3832],
+        name: "Drake",
+        followers: "54,416,812",
+        song: "One Dance"
+    },
+    {
+        location: [42.9849, -81.2453],
+        name: "Justin Bieber",
+        followers: "44,606,973",
+        song: "Love Yourself"
+    },
+    {
+        location: [39.7675, -94.8467],
+        name: "	Eminem",
+        followers: "43,747,833",
+        song: "Lose Yourself"
+    },
+    {
+        location: [13.1132, -59.5988],
+        name: "Rihanna",
+        followers: "42,244,011",
+        song: "Umbrella"
+    },
+    {
+        location: [34.0522, -118.2437],
+        name: "Billie Eilish",
+        followers: "41,792,604",
+        song: "Bad Guy"
+    },
+    {
+        location: [40.3337, -75.9474],
+        name: "Taylor Swift",
+        followers: "38,869,193",
+        song: "Shake if Off"
+    },
+    {
+        location: [36.1699, -115.1398],
+        name: "Imagine Dragon",
+        followers: "33,665,795",
+        song: "Thunder"
+    },
+    {
+        location: [51.5074, - 0.1278],
+        name: "Queen",
+        followers: "33,483,326",
+        song: "	Bohemian Rhapsody"
+    },
+    ];
+
+     // Loop through the artists array and create one marker for each artists
+     for (var i = 0; i < artists.length; i++) {
+        var artist = artists[i];
+        L.marker(artist.location)
+            .bindPopup("<h1>" + artist.name + "</h1> <hr> <h3> # of Followers : " + artist.followers + "<hr>Top Song: "+ artist.song +  "</h3>")
+            .addTo(myMap);
+    }
+};
+
+
+
+window.addEventListener('DOMContentLoaded', init);
---
+++ artists_data/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoibm91cmhhbWRhbiIsImEiOiJja3A0b2VpYm0wNDVwMnVyMDcwbDU1N2RsIn0.UnZOG6Op1Vul7zR6DXBytg"
